b8ef70d0987ea302042fcc6f607e8fdabcfb9f6e
Mikhail Ramanovich <mikhail.ramanovich@standardbank.co.za>
2017-01-06 10:00:54 +0300

    pattern Visitor - PersonnelVisitor added

--- src/main/java/com/mr/gof/visitor/Company.java
+++ src/main/java/com/mr/gof/visitor/Company.java
@@ -49,14 +49,24 @@ public class Company {
     }
 
     public static void main(String[] args){
+
         Company company = new Company();
-        IncomeVisitor incomeVisitor = new IncomeVisitor();
         company.initialize();
+
+        IncomeVisitor incomeVisitor = new IncomeVisitor();
         company.superBoss.accept(incomeVisitor);
         System.out.println("super boss income = " + incomeVisitor.getSuperBossIncome());
         System.out.println("bosses income = " + incomeVisitor.getBossIncome());
         System.out.println("employees income = " + incomeVisitor.getEmployeeIncome());
         System.out.println("total income = " + incomeVisitor.getTotalIncome());
+
+        PersonnelVisitor personnelVisitor = new PersonnelVisitor();
+        company.superBoss.accept(personnelVisitor);
+        System.out.println("super boss amount : " + personnelVisitor.getSuperBossAmount());
+        System.out.println("bosses amount : " + personnelVisitor.getBossAmount());
+        System.out.println("employees amount : " + personnelVisitor.getEmployeeAmount());
+        System.out.println("total personnel amount : " + personnelVisitor.getPersonnelAmount());
+        System.out.println("personnel " + personnelVisitor.getPersonnel());
     }
 
 }
---
+++ src/main/java/com/mr/gof/visitor/PersonnelVisitor.java
@@ -0,0 +1,54 @@
+package com.mr.gof.visitor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Ramanovich on 1/6/2017.
+ */
+public class PersonnelVisitor implements Visitor {
+
+    private int employeeAmount;
+    private int bossAmount;
+    private int superBossAmount;
+
+    private List<String> personnel = new ArrayList<String>();
+
+    @Override
+    public void visit(Employee employee) {
+        employeeAmount++;
+        personnel.add( "Employee: " + employee.getName() );
+    }
+
+    @Override
+    public void visit(Boss boss) {
+        bossAmount++;
+        personnel.add( "Boss: " + boss.getName() );
+    }
+
+    @Override
+    public void visit(SuperBoss superBoss) {
+        superBossAmount++;
+        personnel.add( "Super Boss: " + superBoss.getName() );
+    }
+
+    public int getEmployeeAmount() {
+        return employeeAmount;
+    }
+
+    public int getBossAmount() {
+        return bossAmount;
+    }
+
+    public int getSuperBossAmount() {
+        return superBossAmount;
+    }
+
+    public int getPersonnelAmount(){
+        return employeeAmount + bossAmount + superBossAmount;
+    }
+
+    public String getPersonnel(){
+        return personnel.toString();
+    }
+}
