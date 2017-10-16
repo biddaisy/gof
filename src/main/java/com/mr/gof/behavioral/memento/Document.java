bef42e577a51a537d7d126ee6cba39580939ab2d
Mikhail Ramanovich <mikhail.ramanovich@standardbank.co.za>
2017-10-16 14:45:24 +0300

    pattern Memento

---
+++ src/main/java/com/mr/gof/behavioral/memento/Document.java
@@ -0,0 +1,30 @@
+package com.mr.gof.behavioral.memento;
+
+public class Document {
+
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Memento save(){
+        return new Memento(content);
+    }
+
+    public void restore(Object object){
+        this.content = ((Memento)object).content;
+    }
+
+    private class Memento{
+        private String content;
+        public Memento(String content){
+            this.content = content;
+        }
+    }
+}
+
