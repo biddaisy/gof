0134efcf2e79350c2eb3349f403e0948dc1e1eb6
Mike Romanovich <mike.romanovich@gmail.com>
2020-06-08 16:28:18 +0300

    visitor: test moved to Test

--- src/main/java/com/mr/gof/behavioral/visitor/Company.java
+++ src/main/java/com/mr/gof/behavioral/visitor/Company.java
@@ -23,21 +23,25 @@ public class Company {
         boss2.addEmployee(employee4);
     }
 
-    private SuperBoss createSuperBoss() {
-        SuperBoss superBoss = new SuperBoss();
-        superBoss.setName("SuperBoss");
-        superBoss.setSalary(1000);
-        superBoss.setAnnualBonus(2000);
-        superBoss.setOptionBonus(10000);
+    public SuperBoss getSuperBoss() {
         return superBoss;
     }
 
+    private SuperBoss createSuperBoss() {
+        SuperBoss boss = new SuperBoss();
+        boss.setName("SuperBoss");
+        boss.setSalary(1000);
+        boss.setAnnualBonus(2000);
+        boss.setOptionBonus(10000);
+        return boss;
+    }
+
     private Boss createBoss(String name, int salary, int annualBonus){
-        Boss superBoss = new Boss();
-        superBoss.setName(name);
-        superBoss.setSalary(salary);
-        superBoss.setAnnualBonus(annualBonus);
-        return superBoss;
+        Boss boss = new Boss();
+        boss.setName(name);
+        boss.setSalary(salary);
+        boss.setAnnualBonus(annualBonus);
+        return boss;
     }
 
     private Employee createEmployee(String name, int salary, int incentiveBonus){
@@ -46,27 +50,6 @@ public class Company {
         employee.setSalary(salary);
         employee.setIncentiveBonus(incentiveBonus);
         return employee;
-    }
-
-    public static void main(String[] args){
-
-        Company company = new Company();
-        company.initialize();
-
-        IncomeVisitor incomeVisitor = new IncomeVisitor();
-        company.superBoss.accept(incomeVisitor);
-        System.out.println("super boss income = " + incomeVisitor.getSuperBossIncome());
-        System.out.println("bosses income = " + incomeVisitor.getBossIncome());
-        System.out.println("employees income = " + incomeVisitor.getEmployeeIncome());
-        System.out.println("total income = " + incomeVisitor.getTotalIncome());
-
-        PersonnelVisitor personnelVisitor = new PersonnelVisitor();
-        company.superBoss.accept(personnelVisitor);
-        System.out.println("super boss amount : " + personnelVisitor.getSuperBossAmount());
-        System.out.println("bosses amount : " + personnelVisitor.getBossAmount());
-        System.out.println("employees amount : " + personnelVisitor.getEmployeeAmount());
-        System.out.println("total personnel amount : " + personnelVisitor.getPersonnelAmount());
-        System.out.println("personnel " + personnelVisitor.getPersonnel());
     }
 
 }
---
+++ src/main/java/com/mr/gof/behavioral/visitor/Test.java
@@ -0,0 +1,27 @@
+package com.mr.gof.behavioral.visitor;
+
+public class Test {
+
+
+    public static void main(String[] args){
+
+        Company company = new Company();
+        company.initialize();
+
+        IncomeVisitor incomeVisitor = new IncomeVisitor();
+        company.getSuperBoss().accept(incomeVisitor);
+        System.out.println("super boss income = " + incomeVisitor.getSuperBossIncome());
+        System.out.println("bosses income = " + incomeVisitor.getBossIncome());
+        System.out.println("employees income = " + incomeVisitor.getEmployeeIncome());
+        System.out.println("total income = " + incomeVisitor.getTotalIncome());
+
+        PersonnelVisitor personnelVisitor = new PersonnelVisitor();
+        company.getSuperBoss().accept(personnelVisitor);
+        System.out.println("super boss amount : " + personnelVisitor.getSuperBossAmount());
+        System.out.println("bosses amount : " + personnelVisitor.getBossAmount());
+        System.out.println("employees amount : " + personnelVisitor.getEmployeeAmount());
+        System.out.println("total personnel amount : " + personnelVisitor.getPersonnelAmount());
+        System.out.println("personnel " + personnelVisitor.getPersonnel());
+    }
+
+}
